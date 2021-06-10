5aa6cdd6b4b194efdca4745a9db569c965135a43
asifweb <asifwebhero@gmail.com>
2021-06-10 11:17:11 +0530

    Theme Switcher using Context API

---
+++ five-context-api/src/Context-Api-Part1/App.js
@@ -0,0 +1,45 @@
+import React, { Fragment } from 'react'
+import Provider from './Provider'
+import Context from "./Context"
+
+const Agents = () => {
+  return <AgentOne />
+}
+
+const AgentOne = () => {
+  return <AgentTwo />
+}
+
+const AgentTwo = () => {
+  return <AgentBond />
+}
+
+const AgentBond = () => {
+  return (
+    <Context.Consumer>
+      {
+        (context) => (
+          <Fragment>
+            <h3>Agent Information</h3>
+            <p>Mission Name: {context.data.mname}</p>
+            <p>Mission Status: {context.data.accept} </p>
+            <button onClick={context.isMissionAccepted} >Accept Mission</button>
+          </Fragment>
+        )
+      }
+    </Context.Consumer>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Context API</h1>
+      <Provider>
+        <Agents />
+      </Provider>
+    </div>
+  )
+}
+export default App;
+
---
+++ five-context-api/src/Context-Api-Part1/Problem.js
@@ -0,0 +1,31 @@
+// import React, { useState } from 'react'
+
+// const GrandChild = ({ brand }) => {
+//   return (
+//     <div>
+//       <h1>Grand Child</h1>
+//       <Child brand={brand} />
+//     </div>
+//   )
+// }
+
+// const Child = ({ brand }) => {
+//   return (
+//     <div>
+//       <h1>Child</h1>
+//       <h2>{brand}</h2>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const [brandname] = useState('Amazon');
+//   return (
+//     <div>
+//       <h1>Hello</h1>
+//       <GrandChild brand={brandname} />
+//     </div>
+//   )
+// }
+
+// export default App;
